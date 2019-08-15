48d6310a2237d0e1b73124611ce5f6a779ff94ba
Richard Wang <rlw324@cornell.edu>
2019-08-15 16:19:17 -0400

    more drag progress

--- static/script.js
+++ static/script.js
@@ -1,60 +1,7 @@
-const fill = document.querySelector('.fill');
-const empties = document.querySelectorAll('.empty');
-
-// Fill listeners
-fill.addEventListener('dragstart', dragStart);
-fill.addEventListener('dragend', dragEnd);
-
-// Loop through empty boxes and add listeners
-for (const empty of empties) {
-    empty.addEventListener('dragover', dragOver);
-    empty.addEventListener('dragenter', dragEnter);
-    empty.addEventListener('dragleave', dragLeave);
-    empty.addEventListener('drop', dragDrop);
-}
-
-// Drag Functions
-
-function dragStart() {
-    this.className += ' hold';
-    setTimeout(() => (this.className = 'invisible'), 0);
-    console.log("start");
-}
-
-function dragEnd() {
-    this.className = 'fill';
-    console.log("end");
-}
-
-function dragOver(e) {
-    e.preventDefault();
-    console.log("Over");
-
-}
-
-function dragEnter(e) {
-    e.preventDefault();
-    this.className += ' hovered';
-    console.log("Enter");
-}
-
-function dragLeave() {
-    this.className = 'empty';
-    console.log("Leave");
-}
-
-function dragDrop() {
-    this.className = 'empty';
-    this.append(fill);
-    console.log("Drop");
-}
-
-
-
 // Dragging
 function dragover_handler(ev) {
     console.log("dragOver");
-    ev.currentTarget.style.background = "lightblue";
+    // ev.currentTarget.style.background = "lightblue";
     ev.preventDefault();
 }
 
@@ -73,6 +20,6 @@ function drop_handler(ev) {
 
 function dragend_handler(ev) {
     console.log("dragEnd");
-    ev.target.style.border = "solid black";
+    // ev.target.style.border = "solid black";
     ev.dataTransfer.clearData();
 }
