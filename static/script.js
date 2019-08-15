3e1b4d69b9c002be219cf6f13804913e10f2750d
Richard Wang <rlw324@cornell.edu>
2019-08-14 20:38:13 -0400

    more progress on dragging

--- static/script.js
+++ static/script.js
@@ -1,3 +1,53 @@
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+// Fill listeners
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+// Drag Functions
+
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+    console.log("start");
+}
+
+function dragEnd() {
+    this.className = 'fill';
+    console.log("end");
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    console.log("Over");
+
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+    console.log("Enter");
+}
+
+function dragLeave() {
+    this.className = 'empty';
+    console.log("Leave");
+}
+
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+    console.log("Drop");
+}
 
 
 
